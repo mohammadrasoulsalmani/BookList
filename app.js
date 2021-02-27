a6955ceccfcd3e6cefdc2dd4cafc6055c8ce08f0
mohammad rasoul salmani <mrsa2020block@yahoo.com>
2021-02-27 11:14:39 +0330

    Initial Author Routes

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 if (process.env.NODE_ENV !== 'production') {
     require("dotenv").config();
 }
-
-const express = require("express");
+const express = require("express"); 
 const expressLayouts = require("express-ejs-layouts");
 const app = express();
+// const indexRoutes = require("./routes/index");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
-
+const authoRouter = require("./routes/authors");
 
 app.set("view engine" , "ejs");
 app.set("views" , __dirname + "/views" );
 app.set("layout" , "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit : '10mb' , extended : false }))
 
 mongoose.connect(process.env.DATABASE_URL , { useNewUrlParser: true, useCreateIndex: true , useUnifiedTopology: true  })
 const db = mongoose.connection;
 db.on("error" , error => console.error(error));
-db.once("open" , () => console.log("Connectred to Mongoose "));
+db.once("open" , () => console.log("Connected to Mongoose "));
 
 app.use("/" , indexRouter);
+app.use("/authors" , authoRouter);
 
 
 app.listen(process.env.PORT || 3000);
